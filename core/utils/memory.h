ad2b001b3b745ec31462408105338fd6d05e1d14
mkhan3189 <mkhan3189@gmail.com>
2014-01-17 23:18:16 +1100

    vc fix

--- core/utils/memory.h
+++ core/utils/memory.h
@@ -36,6 +36,7 @@ inline void turnToNullPtr(T*& pointer, T2*&... pointers) {
 }
 #else
 // Visual C++ (pre VS-2013) does not support varidiac templates hence we overload
+
 template <typename T, typename T2>
 inline void deleteAll(T*& pointer, T2*& pointer2) {
     memory::deleteAll(pointer);
@@ -68,6 +69,83 @@ inline void deleteAll(T*& pointer, T2*& pointer2, T3*& pointer3,
     memory::deleteAll(pointer5);
 }
 
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6>
+inline void deleteAll(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6) {
+    memory::deleteAll(pointer);
+    memory::deleteAll(pointer2);
+    memory::deleteAll(pointer3);
+    memory::deleteAll(pointer4);
+    memory::deleteAll(pointer5);
+    memory::deleteAll(pointer6);
+}
+
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7>
+inline void deleteAll(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6, T7*& pointer7) {
+    memory::deleteAll(pointer);
+    memory::deleteAll(pointer2);
+    memory::deleteAll(pointer3);
+    memory::deleteAll(pointer4);
+    memory::deleteAll(pointer5);
+    memory::deleteAll(pointer6);
+    memory::deleteAll(pointer7);
+}
+
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8>
+inline void deleteAll(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6, T7*& pointer7, T8*& pointer8) {
+    memory::deleteAll(pointer);
+    memory::deleteAll(pointer2);
+    memory::deleteAll(pointer3);
+    memory::deleteAll(pointer4);
+    memory::deleteAll(pointer5);
+    memory::deleteAll(pointer6);
+    memory::deleteAll(pointer7);
+    memory::deleteAll(pointer8);
+}
+
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9>
+inline void deleteAll(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6, T7*& pointer7, T8*& pointer8, 
+        T9*& pointer9) {
+    memory::deleteAll(pointer);
+    memory::deleteAll(pointer2);
+    memory::deleteAll(pointer3);
+    memory::deleteAll(pointer4);
+    memory::deleteAll(pointer5);
+    memory::deleteAll(pointer6);
+    memory::deleteAll(pointer7);
+    memory::deleteAll(pointer8);
+    memory::deleteAll(pointer9);
+}
+
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9, typename T10>
+inline void deleteAll(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6, T7*& pointer7, T8*& pointer8, 
+        T9*& pointer9, T10*& pointer10) {
+    memory::deleteAll(pointer);
+    memory::deleteAll(pointer2);
+    memory::deleteAll(pointer3);
+    memory::deleteAll(pointer4);
+    memory::deleteAll(pointer5);
+    memory::deleteAll(pointer6);
+    memory::deleteAll(pointer7);
+    memory::deleteAll(pointer8);
+    memory::deleteAll(pointer9);
+    memory::deleteAll(pointer10);
+}
+
 template <typename T, typename T2>
 inline void turnToNullPtr(T*& pointer, T2*& pointer2) {
     memory::turnToNullPtr(pointer);
@@ -98,6 +176,83 @@ inline void turnToNullPtr(T*& pointer, T2*& pointer2, T3*& pointer3,
     memory::turnToNullPtr(pointer3);
     memory::turnToNullPtr(pointer4);
     memory::turnToNullPtr(pointer5);
+}
+
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6>
+inline void turnToNullPtr(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6) {
+    memory::turnToNullPtr(pointer);
+    memory::turnToNullPtr(pointer2);
+    memory::turnToNullPtr(pointer3);
+    memory::turnToNullPtr(pointer4);
+    memory::turnToNullPtr(pointer5);
+    memory::turnToNullPtr(pointer6);
+}
+
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7>
+inline void turnToNullPtr(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6, T7*& pointer7) {
+    memory::turnToNullPtr(pointer);
+    memory::turnToNullPtr(pointer2);
+    memory::turnToNullPtr(pointer3);
+    memory::turnToNullPtr(pointer4);
+    memory::turnToNullPtr(pointer5);
+    memory::turnToNullPtr(pointer6);
+    memory::turnToNullPtr(pointer7);
+}
+
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8>
+inline void turnToNullPtr(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6, T7*& pointer7, T8*& pointer8) {
+    memory::turnToNullPtr(pointer);
+    memory::turnToNullPtr(pointer2);
+    memory::turnToNullPtr(pointer3);
+    memory::turnToNullPtr(pointer4);
+    memory::turnToNullPtr(pointer5);
+    memory::turnToNullPtr(pointer6);
+    memory::turnToNullPtr(pointer7);
+    memory::turnToNullPtr(pointer8);
+}
+
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9>
+inline void turnToNullPtr(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6, T7*& pointer7, T8*& pointer8, 
+        T9*& pointer9) {
+    memory::turnToNullPtr(pointer);
+    memory::turnToNullPtr(pointer2);
+    memory::turnToNullPtr(pointer3);
+    memory::turnToNullPtr(pointer4);
+    memory::turnToNullPtr(pointer5);
+    memory::turnToNullPtr(pointer6);
+    memory::turnToNullPtr(pointer7);
+    memory::turnToNullPtr(pointer8);
+    memory::turnToNullPtr(pointer9);
+}
+
+template <typename T, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9, typename T10>
+inline void turnToNullPtr(T*& pointer, T2*& pointer2, T3*& pointer3, 
+        T4*& pointer4, T5*& pointer5, 
+        T6*& pointer6, T7*& pointer7, T8*& pointer8, 
+        T9*& pointer9, T10*& pointer10) {
+    memory::turnToNullPtr(pointer);
+    memory::turnToNullPtr(pointer2);
+    memory::turnToNullPtr(pointer3);
+    memory::turnToNullPtr(pointer4);
+    memory::turnToNullPtr(pointer5);
+    memory::turnToNullPtr(pointer6);
+    memory::turnToNullPtr(pointer7);
+    memory::turnToNullPtr(pointer8);
+    memory::turnToNullPtr(pointer9);
+    memory::turnToNullPtr(pointer10);
 }
 #endif // defined(_MSC_VER)
 } // namespace memory
